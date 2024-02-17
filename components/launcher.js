1f33f7ec05fe3754d2253de26bbf4fd671f16f80
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-17 17:35:10 -0600

    Update launcher.js

--- components/launcher.js
+++ components/launcher.js
@@ -5,42 +5,39 @@ const downloader = require('./downloader');
 const { v4: uuidv4 } = require('uuid');
 
 class Launcher {
+  constructor() {
+    this.downloader = new downloader(this);
+  }
 
   createProfile(root) {
-    if(!fs.existsSync(path.join(root, 'launcher_profiles.json'))) {
-      fs.writeFileSync(path.resolve(path.join(root, 'launcher_profiles.json')), JSON.stringify({ profiles: {}  }));
+    if (!fs.existsSync(path.join(root, 'launcher_profiles.json'))) {
+      fs.writeFileSync(path.resolve(path.join(root, 'launcher_profiles.json')), JSON.stringify({ profiles: {} }));
     }
   }
 
   encontrarArchivosJAR(directorio, files, ver) {
     const archivos = fs.readdirSync(directorio);
     let archivosJARString = '';
-  
+
     archivos.forEach((archivo) => {
       const rutaCompleta = path.join(directorio, archivo);
       if (fs.statSync(rutaCompleta).isDirectory()) {
         archivosJARString += this.encontrarArchivosJAR(rutaCompleta, files, ver);
       } else {
-        if(['1.14', '1.14.1', '1.14.2', '1.14.3'].includes(ver)) {
-          if(path.extname(archivo) === '.jar' && files.includes(archivo)) {
-            archivosJARString += rutaCompleta + ';';
-            // console.log(archivo)
-          }
-        } else {
-          if (path.extname(archivo) === '.jar' && files.includes(archivo) && !archivo.includes('3.2.1')) {
-            archivosJARString += rutaCompleta + ';';
-            // console.log(archivo)
-          }
+        if (ver.includes('1.14') && path.extname(archivo) === '.jar' && files.includes(archivo)) {
+          archivosJARString += rutaCompleta + ';';
+        } else if (!ver.includes('1.14') && path.extname(archivo) === '.jar' && files.includes(archivo) && !archivo.includes('3.2.1')) {
+          archivosJARString += rutaCompleta + ';';
         }
       }
     });
-    
+
     return archivosJARString;
   }
 
   auth(root, us) {
     try {
-      const fil = JSON.parse(fs.readFileSync(path.join(root, 'usercache.json'), { encoding: 'utf-8'}))
+      const fil = JSON.parse(fs.readFileSync(path.join(root, 'usercache.json'), { encoding: 'utf-8' }))
       return fil.find(x => x.name === us).uuid;
     } catch (error) {
       console.log("NO SE HAN ENCONTRADO USUARIOS, CREANDO UNO");
@@ -49,40 +46,43 @@ class Launcher {
   }
 
   launch(options) {
-    
-    this.downloader = new downloader(this);
-
     const minM = options.memory.min;
     const maxM = options.memory.max;
     const rootPath = options.gameDirectory;
     const version = options.version.match(/\b1\.\d+(\.\d+)?\b/g)[0];
-    const custom = options.version !== version ? options.version : null
+    const custom = options.version !== version ? options.version : null;
 
     const username = options.username;
-    const file = JSON.parse(fs.readFileSync(path.join(rootPath, this.downloader.versions, version, `${version}.json`), { encoding: 'utf-8'}));
+    const file = JSON.parse(fs.readFileSync(path.join(rootPath, this.downloader.versions, version, `${version}.json`), { encoding: 'utf-8' }));
     this.createProfile(rootPath);
     const uuid = this.auth(rootPath, username);
-    const reqLibs = [];
-    file.libraries.forEach(element => {
-      if(element.downloads.artifact !== undefined) reqLibs.push(path.basename(element.downloads.artifact.path))
-    })
+    const reqLibs = file.libraries.filter(element => element.downloads && element.downloads.artifact).map(element => path.basename(element.downloads.artifact.path));
     let mainClass = file.mainClass;
     let gameArgs = file.minecraftArguments ? file.minecraftArguments.split(' ') : file.arguments.game;
-    if(custom !== null) {
+    if (custom !== null) {
       const customFile = JSON.parse(fs.readFileSync(path.join(rootPath, this.downloader.versions, custom, `${custom}.json`), { encoding: 'utf-8' }));
       customFile.libraries.forEach(element => {
-        reqLibs.push(element.name.split(':').slice(-2).join("-").concat('.jar'))
+        if (element.downloads && element.downloads.artifact) {
+          reqLibs.push(path.basename(element.downloads.artifact.path));
+        }
       });
       mainClass = customFile.mainClass;
-      if(!customFile.arguments) {
+      if (!customFile.arguments) {
         gameArgs = customFile.minecraftArguments.split(' ');
       } else {
-        gameArgs.push(customFile.arguments.game)
+        gameArgs.push(customFile.arguments.game);
       }
-      if(fs.existsSync(path.resolve(path.join(rootPath, 'options.txt')))) fs.unlinkSync(path.resolve(path.join(rootPath, 'options.txt')));
+      if (fs.existsSync(path.resolve(path.join(rootPath, 'options.txt')))) {
+        fs.unlinkSync(path.resolve(path.join(rootPath, 'options.txt')));
+      }
     }
     let libs = this.encontrarArchivosJAR(path.resolve(path.join(rootPath, this.downloader.libraries)), reqLibs, version);
-    custom === null || custom.includes('fabric') ? libs += path.resolve(path.join(rootPath, this.downloader.versions, version, `${version}.jar`)) : libs += path.resolve(path.join(rootPath, this.downloader.versions, custom, `${custom}.jar`));
+    if (custom === null || custom.includes('fabric')) {
+      libs += path.resolve(path.join(rootPath, this.downloader.versions, version, `${version}.jar`));
+    } else {
+      libs += path.resolve(path.join(rootPath, this.downloader.versions, custom, `${custom}.jar`));
+    }
+
     const fields = {
       '${auth_access_token}': uuid,
       '${auth_session}': uuid,
@@ -100,33 +100,26 @@ class Launcher {
       '${clientid}': uuid,
       '${resolution_width}': 856,
       '${resolution_height}': 482
-    }
+    };
 
     let args = [
-      `-Djava.library.path=${path.resolve(path.join(rootPath, this.downloader.natives, version)) ? path.resolve(path.join(rootPath, this.downloader.natives, version)) : path.resolve(path.join(rootPath))}`,
+      `-Djava.library.path=${path.resolve(path.join(rootPath, this.downloader.natives, version)) || path.resolve(rootPath)}`,
       `-Xmx${maxM}`,
       `-Xms${minM}`,
       '-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump',
       '-cp',
       libs,
       mainClass,
-      gameArgs.flat()
+      ...gameArgs
     ];
 
-    args = args.reduce((acc, curr) => acc.concat(curr), [])
+    args = args.map(arg => fields[arg] ? fields[arg] : arg);
 
-    for (let i = 0; i < args.length; i++) {
-      if (Object.keys(fields).includes(args[i])) {
-        args[i] = fields[args[i]]; 
-      }
-    }
-
-    // console.log(args)
-    const spawnRoot = path.resolve(rootPath)
-    const minecraft = spawn('java', args, { cwd: spawnRoot })
+    const spawnRoot = path.resolve(rootPath);
+    const minecraft = spawn('java', args, { cwd: spawnRoot });
     console.log(`INICIANDO MINECRAFT VERSION: ${version}`);
-    minecraft.stdout.on('data', (data) => console.log(data.toString()))
-    minecraft.stderr.on('data', (data) => console.log(data.toString()))
+    minecraft.stdout.on('data', (data) => console.log(data.toString()));
+    minecraft.stderr.on('data', (data) => console.log(data.toString()));
   }
 }
 
