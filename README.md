79644ba61b1a662af2a8c2919fa0b9b2ce3bd7b8
Alexander Barreto <adguerrero59@gmail.com>
2024-02-20 00:53:49 -0500

    1.0 Forge Support

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # ADLAUNCHER-CORE | MINECRAFT LAUNCHER WITH NODE JS
 
-This is a simple minecraft-core for download and play minecraft with node.js. Phase Alpha / Simple minecraft-core para descargar y jugar minecraft con node.js en fase Alpha.
+This is a simple minecraft-core for download and play minecraft with node.js. / Simple minecraft-core para descargar y jugar minecraft con node.js.
 
 ---
 
-Este en un proyecto desarrollado en Node encargado de conseguir los archivos para ejectuar minecraft Vanilla y OptiFine (por el momento). En el futuro se estará actualizando el paquete.
+Este en un proyecto desarrollado en Node encargado de conseguir los archivos para ejectuar minecraft Vanilla, OptiFine, Forge y Fabric. Se estará actualizando el paquete tomando en cuenta sugerencias y reportes de errores.
 
-## Notes
-El paquete está en versión Beta, las versiones habilitadas son `1.8` hacia arriba.
-
-Mientras que para versiones OptiFine están habilitadas desde la versión `1.8.9`.
+El paquete finalmente se encuentra en su primera versión oficial `1.0`, con soporte a OptiFine, Forge y Fabric.
 
 El proyecto no está habilitado para jugar en servidores premium. (No se apoya a la piratería).
+
+## Requirements
+- [Java 8](https://www.java.com/en/download/manual.jsp) (Solo para versiones menores a la 1.16.5 de forge)
+- [Java 17](https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html)
+- [Node.js](https://nodejs.org/en)
 
 ## Usage
 
@@ -49,7 +51,7 @@ const launchOptions = {
 launcher.launch(launchOptions); // Inicia Minecraft con las opciones declaradas
 ```
 
-### OptiFine
+### Play with OptiFine
 En el caso que quieras jugar con optimización y necesites instalar OptiFine, deberás instalarlo de manera manual y especificar en `version` la carpeta de OptiFine instalada.
 
 Si tienes problemas al instalar `Fabric` de forma manual, puedes ver [Cómo instalar OptiFine en MINECRAFT](https://youtu.be/hPIQIweUXL8?si=ZhKtysEGmv2Ijsn5)
@@ -72,10 +74,10 @@ const launchOptions = {
 launcher.launch(launchOptions); // Inicia Minecraft con las opciones declaradas
 ```
 
-### Fabric
+### Play with Fabric
 En el caso de que quieras jugar con mods, ya está disponible en `adlauncher-core` el soporte de fabric.
 
-Debes instalarlo de forma manual al igual que OptiFine (ya luego lo haré automatizado) y especificar en `version` la carpeta de fabric instalada.
+Debes instalarlo de forma manual al igual que OptiFine y especificar en `version` la carpeta de fabric instalada.
 
 Si tienes problemas al instalar `Fabric` de forma manual, puedes ver [Cómo INSTALAR FABRIC y MODS en MINECRAFT](https://youtu.be/taUC6R_LiOE?si=Ewz36e0YfV0LOWAp)
 ```js
@@ -96,6 +98,45 @@ const launchOptions = {
 
 launcher.launch(launchOptions); // Inicia Minecraft con las opciones declaradas
 ```
+
+### Play with Forge
+En el caso de que quieras jugar con mods y no te gusta `fabric`, ya está disponible finalmente en `adlauncher-core` el soporte de forge.
+
+Debes instalarlo de forma manual al igual que OptiFine y Fabric, especifica en `version` la carpeta de forge instalada.
+
+Si tienes problemas al instalar `Forge` de forma manual, puedes ver ESPERA A QUE SUBA EL VIDEO.
+
+```js
+const { Launcher } = require('adlauncher-core');
+
+const launcher = new Launcher();
+
+// Declara las opciones con las que vas a lanzar una versión de Minecraft
+const launchOptions = {
+  username: 'dani_adbg', // Ingresa tu nombre de usuario
+  version: '1.20-forge-46.0.14', // Ingresa la versión de Forge
+  gameDirectory: './minecraft', // Ingresa el directorio donde tienes descargado Minecraft
+  memory: { // Define la memoria que quieras usar
+    min: '2G', // Mínimo de memoria
+    max: '6G'  // Máximo de memoria
+  }
+}
+
+launcher.launch(launchOptions); // Inicia Minecraft con las opciones declaradas
+```
+
+#### Importante
+Forge solo está disponible desde la versión `1.12` hasta la más reciente.
+
+Para jugar desde la `1.12` hasta la `1.16.5` se necesita `Java 8 x64` [Descarga Java 8](https://www.java.com/en/download/manual.jsp)
+
+Mientras que para jugar desde la versión `1.17` se necesita `Java 17` [Descarga Java 17](https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html)
+
+Al momento de instalarlos, asegurate que los directorios sean los que traen por defecto, caso contrario pueden ocurrir errores.
+
+Forge fue un total dolor de cabeza. Si ocurre un error, reportalo de inmediato en nuestro [Server de Discord](https://discord.gg/a93w5NpBR9).
+
+---
 
 ## Support
 [![](https://dcbadge.vercel.app/api/server/a93w5NpBR9)](https://discord.gg/a93w5NpBR9)
--- components/launcher.js
+++ components/launcher.js
@@ -5,19 +5,22 @@ const downloader = require('./downloader');
 const { v4: uuidv4 } = require('uuid');
 
 class Launcher {
+  constructor() {
+    this.downloader = new downloader(this);
+  }
 
   createProfile(root) {
-    if(!fs.existsSync(path.join(root, 'launcher_profiles.json'))) {
-      fs.writeFileSync(path.resolve(path.join(root, 'launcher_profiles.json')), JSON.stringify({ profiles: {}  }));
+    if(!fs.existsSync(path.resolve(root, 'launcher_profiles.json'))) {
+      fs.writeFileSync(path.resolve(root, 'launcher_profiles.json'), JSON.stringify({ profiles: {} }));
     }
   }
 
   encontrarArchivosJAR(directorio, files, ver) {
     const archivos = fs.readdirSync(directorio);
     let archivosJARString = '';
-  
+
     archivos.forEach((archivo) => {
-      const rutaCompleta = path.join(directorio, archivo);
+      const rutaCompleta = path.resolve(directorio, archivo);
       if (fs.statSync(rutaCompleta).isDirectory()) {
         archivosJARString += this.encontrarArchivosJAR(rutaCompleta, files, ver);
       } else {
@@ -34,13 +37,12 @@ class Launcher {
         }
       }
     });
-    
     return archivosJARString;
   }
 
   auth(root, us) {
     try {
-      const fil = JSON.parse(fs.readFileSync(path.join(root, 'usercache.json'), { encoding: 'utf-8'}))
+      const fil = JSON.parse(fs.readFileSync(path.resolve(root, 'usercache.json'), { encoding: 'utf-8'}))
       return fil.find(x => x.name === us).uuid;
     } catch (error) {
       console.log("NO SE HAN ENCONTRADO USUARIOS, CREANDO UNO");
@@ -48,41 +50,66 @@ class Launcher {
     }
   }
 
-  launch(options) {
+  async launch(options) {
     
-    this.downloader = new downloader(this);
-
     const minM = options.memory.min;
     const maxM = options.memory.max;
     const rootPath = options.gameDirectory;
     const version = options.version.match(/\b1\.\d+(\.\d+)?\b/g)[0];
-    const custom = options.version !== version ? options.version : null
-
+    const custom = options.version !== version ? options.version : null;
     const username = options.username;
-    const file = JSON.parse(fs.readFileSync(path.join(rootPath, this.downloader.versions, version, `${version}.json`), { encoding: 'utf-8'}));
+    const file = JSON.parse(fs.readFileSync(path.resolve(rootPath, this.downloader.versions, version, `${version}.json`), { encoding: 'utf-8'}));
+
     this.createProfile(rootPath);
+
     const uuid = this.auth(rootPath, username);
-    const reqLibs = [];
-    file.libraries.forEach(element => {
-      if(element.downloads.artifact !== undefined) reqLibs.push(path.basename(element.downloads.artifact.path))
-    })
+    const reqLibs = file.libraries.filter(element => element.downloads && element.downloads.artifact).map(element => path.basename(element.downloads.artifact.path));
     let mainClass = file.mainClass;
     let gameArgs = file.minecraftArguments ? file.minecraftArguments.split(' ') : file.arguments.game;
+    
+    let jvm = [
+      `-Djava.library.path=${path.resolve(rootPath, this.downloader.natives, version)}`,
+      `-Xmx${maxM}`,
+      `-Xms${minM}`,
+      '-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump'
+    ]
+
     if(custom !== null) {
-      const customFile = JSON.parse(fs.readFileSync(path.join(rootPath, this.downloader.versions, custom, `${custom}.json`), { encoding: 'utf-8' }));
+      const customFile = JSON.parse(fs.readFileSync(path.resolve(rootPath, this.downloader.versions, custom, `${custom}.json`), { encoding: 'utf-8' }));
+
       customFile.libraries.forEach(element => {
         reqLibs.push(element.name.split(':').slice(-2).join("-").concat('.jar'))
       });
+
       mainClass = customFile.mainClass;
+
       if(!customFile.arguments) {
         gameArgs = customFile.minecraftArguments.split(' ');
       } else {
-        gameArgs.push(customFile.arguments.game)
+        if(customFile.arguments.jvm) {
+          jvm.push(...customFile.arguments.jvm);
+        }
+        gameArgs.push(...customFile.arguments.game);
       }
-      if(fs.existsSync(path.resolve(path.join(rootPath, 'options.txt')))) fs.unlinkSync(path.resolve(path.join(rootPath, 'options.txt')));
+
+      if(fs.existsSync(path.resolve(rootPath, 'options.txt'))) {
+        fs.unlinkSync(path.resolve(rootPath, 'options.txt'));
+      }
+      
+      if(custom.includes('forge') && version.includes('1.20')) {
+        const matches = custom.split('-');
+        const forgeVersion = matches[matches.length - 1].replace('forge', '');
+        const resultado = `forge-${version}-${forgeVersion}-universal.jar`;
+        const resultClient = `forge-${version}-${forgeVersion}-client.jar`;
+        reqLibs.push(resultado, resultClient);
+        if(['1.20', '1.20.1'].includes(version)) {
+          reqLibs.push('mergetool-1.1.5-api.jar');
+        }
+      }
     }
-    let libs = this.encontrarArchivosJAR(path.resolve(path.join(rootPath, this.downloader.libraries)), reqLibs, version);
-    custom === null || custom.includes('fabric') ? libs += path.resolve(path.join(rootPath, this.downloader.versions, version, `${version}.jar`)) : libs += path.resolve(path.join(rootPath, this.downloader.versions, custom, `${custom}.jar`));
+    
+    let libs = this.encontrarArchivosJAR(path.resolve(rootPath, this.downloader.libraries), reqLibs, version);
+    libs += path.resolve(rootPath, this.downloader.versions, version, `${version}.jar`);
     const fields = {
       '${auth_access_token}': uuid,
       '${auth_session}': uuid,
@@ -94,39 +121,54 @@ class Launcher {
       '${version_name}': version,
       '${assets_index_name}': version,
       '${game_directory}': path.resolve(rootPath),
-      '${assets_root}': path.resolve(path.join(rootPath, this.downloader.assets)),
-      '${game_assets}': path.resolve(path.join(rootPath, this.downloader.assets)),
+      '${assets_root}': path.resolve(rootPath, this.downloader.assets),
+      '${game_assets}': path.resolve(rootPath, this.downloader.assets),
       '${version_type}': 'release',
       '${clientid}': uuid,
       '${resolution_width}': 856,
-      '${resolution_height}': 482
+      '${resolution_height}': 482,
+      library_directory: (path.resolve(rootPath, this.downloader.libraries)).split(path.sep).join('/'),
+      version_name: version,
+      classpath_separator: ';'
+    }
+
+    jvm = jvm.map(str => str.replace(/\$\{(\w+)\}/g, (match, p1) => fields[p1] || match));
+
+    const parV = parseInt(version.split('.')[1]);
+    if (parV >= 17) {
+      jvm.push('-Dlog4j2.formatMsgNoLookups=true');
+    } else if (parV < 17 && parV > 11) {
+      if(!fs.existsSync(path.resolve(rootPath, 'log4j2_112-116.xml'))) {
+        await this.downloader.down('https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml', rootPath, 'log4j2_112-116.xml');
+      };
+      jvm.push('-Dlog4j.configurationFile=log4j2_112-116.xml');
+    } else {
+      if(!fs.existsSync(path.resolve(rootPath, 'log4j2_17-111.xml'))) {
+        await this.downloader.down('https://launcher.mojang.com/v1/objects/4bb89a97a66f350bc9f73b3ca8509632682aea2e/log4j2_17-111.xml', rootPath, 'log4j2_17-111.xml');
+      };
+      jvm.push('-Dlog4j.configurationFile=log4j2_17-111.xml');
     }
 
     let args = [
-      `-Djava.library.path=${path.resolve(path.join(rootPath, this.downloader.natives, version)) ? path.resolve(path.join(rootPath, this.downloader.natives, version)) : path.resolve(path.join(rootPath))}`,
-      `-Xmx${maxM}`,
-      `-Xms${minM}`,
-      '-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump',
+      ...jvm,
       '-cp',
       libs,
       mainClass,
-      gameArgs.flat()
+      ...gameArgs
     ];
-
-    args = args.reduce((acc, curr) => acc.concat(curr), [])
-
-    for (let i = 0; i < args.length; i++) {
-      if (Object.keys(fields).includes(args[i])) {
-        args[i] = fields[args[i]]; 
-      }
-    }
-
+    
+    args = args.map(arg => fields[arg] ? fields[arg] : arg);    
     // console.log(args)
-    const spawnRoot = path.resolve(rootPath)
-    const minecraft = spawn('java', args, { cwd: spawnRoot })
+    let java = 'java';
+    if(custom && custom.includes('forge') && parV < 16) {
+      java = 'C:/Program Files/Java/jre-1.8/bin/java.exe';
+      console.log(`USANDO JAVA 8`);
+    }
+    const spawnRoot = path.resolve(rootPath);
+    const minecraft = spawn(java, args, { cwd: spawnRoot });
     console.log(`INICIANDO MINECRAFT VERSION: ${version}`);
-    minecraft.stdout.on('data', (data) => console.log(data.toString()))
-    minecraft.stderr.on('data', (data) => console.log(data.toString()))
+    minecraft.stdout.on('data', (data) => console.log(data.toString()));
+    minecraft.stderr.on('data', (data) => console.log(data.toString()));
   }
 }
 
